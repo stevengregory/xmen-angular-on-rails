2048cbe13fec1abf5bcecfbb9ecb6f096769c0e9
Steven Gregory <sgregory@gmail.com>
2016-12-25 20:24:11 -0600

    Added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Mutant.create([
+  { name: 'Xavier', power: 'Vast telepathic abilities, intellect and offensive psychic attacks' },
+  { name: 'Iceman', power: 'Cold and ice manipulation' },
+  { name: 'Cyclops', power: 'Optic force blasts and master tactician' },
+  { name: 'Beast', power: 'Enhanced strength, agility and intellect' },
+  { name: 'Wolverine', power: 'Healing factor, superhuman senses, adamantium skeleton and claws' },
+  { name: 'Storm', power: 'Weather manipulation and flight' },
+  { name: 'Colossus', power: 'Steel body transformation, superhuman strength, stamina and durability' },
+  { name: 'Nightcrawler', power: 'Teleportation, enhanced flexibility and speed' },
+  { name: 'Archangel', power: 'Flight and razor sharp feather projection' },
+  { name: 'Gambit', power: 'Accelerating regeneration, kinetic energy control and manipulation' },
+  { name: 'Rogue', power: 'Absorption of powers, super strength and flight' },
+  { name: 'Bishop', power: 'Energy absorption and redirection' },
+  { name: 'Jubilee', power: 'Pyrotechnic energy plasmoid generation' },
+  { name: 'Psylocke', power: 'Telekinesis, telekinetic katana, telepathy and precognition' }
+])
