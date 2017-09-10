377c799649312fe72ae1a7ac6621e495fabb9dd2
Steven Gregory <sgregory@gmail.com>
2017-09-10 15:29:34 -0500

    Updated mutant service & added catch

--- public/app/mutant-list/mutant-list.service.ts
+++ public/app/mutant-list/mutant-list.service.ts
@@ -3,6 +3,8 @@ import { Headers, Http, Response } from '@angular/http';
 
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 import { Mutant } from './mutant';
 
@@ -12,9 +14,13 @@ export class MutantListService {
 
   constructor(private http: Http) {}
 
-  getMutants(): Observable<Array<Mutant>> {
+  getMutants(): Observable<Mutant[]> {
     return this.http
       .get(this.apiUrl)
-      .map((response: Response) => response.json() as Mutant[]);
+      .map((res: Response) => res.json() as Mutant[])
+      .catch((error: any) => {
+        console.error('Magneto has caused an error', error);
+        return Observable.throw(error.message || error);
+      });
   }
 }
